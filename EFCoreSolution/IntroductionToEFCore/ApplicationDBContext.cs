707eb110b8b377d82b4e0ca4a7b6d91263235331
Hellentorresr <htorres1@ucenfotec.ac.cr>
2023-08-04 15:27:05 -0600

    Creating the project

---
+++ EFCoreSolution/IntroductionToEFCore/ApplicationDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IntroductionToEFCore
+{
+    public class ApplicationDBContext : DbContext 
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options) { 
+            
+        }
+    }
+}

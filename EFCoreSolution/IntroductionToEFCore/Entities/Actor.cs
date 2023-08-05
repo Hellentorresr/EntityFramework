e83c79406b5b02b7ec3fe8a697a560f4f9ad791a
Hellentorresr <htorres1@ucenfotec.ac.cr>
2023-08-04 20:19:12 -0600

    Adding Entities and their migrations

---
+++ EFCoreSolution/IntroductionToEFCore/Entities/Actor.cs
@@ -0,0 +1,10 @@
+﻿namespace IntroductionToEFCore.Entities
+{
+    public class Actor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public decimal Furtune { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
---
+++ EFCoreSolution/IntroductionToEFCore/Entities/FilmGenre.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IntroductionToEFCore.Entities
+{
+    public class FilmGenre
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!; //wont be null, just to avoid getting the alert
+    }  
+}

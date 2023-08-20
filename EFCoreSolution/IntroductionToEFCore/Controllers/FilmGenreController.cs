1239366f3da04ebc1e281931ded5560f775ebe8a
Hellentorresr <htorres1@ucenfotec.ac.cr>
2023-08-19 18:06:51 -0600

    Adding controllers

---
+++ EFCoreSolution/IntroductionToEFCore/Controllers/FilmGenreController.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using DTOs;
+using IntroductionToEFCore.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntroductionToEFCore.Controllers
+{
+    [ApiController]
+    [Route("api/filmgenre")]
+    public class FilmGenreController : ControllerBase
+    {
+        private readonly ApplicationDBContext _dbContext;
+        private readonly IMapper mapper;
+
+        public FilmGenreController(ApplicationDBContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            this.mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(FilmGenreDTO filmGenreDTO)
+        {
+            //mapping from Entity to DTO
+            var _filmGenre = mapper.Map<FilmGenre>(filmGenreDTO);
+            //Entity framework work code to insert
+            _dbContext.Add(_filmGenre);
+
+            //saving the changes in the database
+            await _dbContext.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        //Adding many records at the same time 
+        [HttpPost("many")]
+        public async Task<ActionResult> Post(FilmGenreDTO[] filmGenresDTO)
+        {
+            var genres = mapper.Map<FilmGenre[]>(filmGenresDTO);
+            _dbContext.AddRange(genres); //AddRange method is used to add a list of entities
+            await _dbContext.SaveChangesAsync(); //save the changes in the database
+            return Ok();
+        }
+
+    }
+}
---
+++ EFCoreSolution/IntroductionToEFCore/Controllers/MovieController.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using DTOs;
+using IntroductionToEFCore.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntroductionToEFCore.Controllers
+{
+    [ApiController]
+    [Route("api/movies")]
+  
+    public class MovieController : ControllerBase
+    {
+        private readonly ApplicationDBContext context;
+        private readonly IMapper mapper;
+
+        public MovieController(ApplicationDBContext context, IMapper mapper) 
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateMovie(MovieCreationDTO movieDTO)
+        {
+            var movie = mapper.Map<Movie>(movieDTO);
+            
+            //when there's not a junction table
+            if(movie.FilmGenres is not null)
+            {
+                foreach(var genre in movie.FilmGenres)
+                {
+                   context.Entry(genre).State = EntityState.Unchanged; //the Entry method is useful for working with entities that are tracked by the DbContext, as well as entities that are not yet tracked but you want to start tracking them in a specific state.
+                }
+            }
+
+            //when there's a junction table
+            if(movie.MovieActors is not null)
+            {
+              for(int i = 0;  i <movie.MovieActors.Count; i++)
+                {
+                    movie.MovieActors[i].SearchOrder = i + 1; //in the same order the actors are being receive they will be save
+                }
+            }
+
+            context.Add(movie);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}

5c27cddf62a40b2cd8cbb3f9d60883b3bec875ce
Hellentorresr <htorres1@ucenfotec.ac.cr>
2023-08-19 20:34:46 -0600

    Updating the project

---
+++ EFCoreSolution/IntroductionToEFCore/UtilitiesAutoMapper/AutoMapperProfiles.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using DTOs;
+using IntroductionToEFCore.Entities;
+
+namespace IntroductionToEFCore.UtilitiesAutoMapper
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles() {
+
+            CreateMap<FilmGenreDTO, FilmGenre>();
+
+            CreateMap<ActorDTO, Actor>();
+
+            //mapping from type MovieCreationDTO to Movie
+            CreateMap<MovieCreationDTO, Movie>().ForMember(entity => entity.FilmGenres,
+                dto => dto.MapFrom(field => field.Genres.Select(Id => new FilmGenre { Id = Id})));
+
+            CreateMap<MovieActorCreationDTO, MovieActor>();
+        }
+    }
+}

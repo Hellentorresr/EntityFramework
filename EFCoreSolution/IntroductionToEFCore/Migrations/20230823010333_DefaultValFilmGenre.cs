d7e269129c26049ca348c657821b1269b7def960
Hellentorresr <htorres1@ucenfotec.ac.cr>
2023-08-22 19:06:16 -0600

    Adding Migration for FilmGenres default values

---
+++ EFCoreSolution/IntroductionToEFCore/Migrations/20230823010333_DefaultValFilmGenre.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace IntroductionToEFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class DefaultValFilmGenre : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "FilmGenres",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 9, "Fiction" },
+                    { 10, "Animation" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "FilmGenres",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "FilmGenres",
+                keyColumn: "Id",
+                keyValue: 10);
+        }
+    }
+}

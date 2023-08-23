06b9a8a62fa7f3ef63267b355b6fc1b4f676bbb9
Hellentorresr <htorres1@ucenfotec.ac.cr>
2023-08-22 19:59:14 -0600

    Adding InitialSeeding class

---
+++ EFCoreSolution/IntroductionToEFCore/Migrations/20230823015519_DataSeedingInGeneral.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace IntroductionToEFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataSeedingInGeneral : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Actors",
+                columns: new[] { "Id", "Birthday", "Furtune", "Name" },
+                values: new object[,]
+                {
+                    { 3, new DateTime(1948, 12, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), 15000m, "Samuel L. Jackson" },
+                    { 4, new DateTime(1965, 4, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), 18000m, "Robert Downey JR." }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "MovieReleaseDate", "Name", "OnListing" },
+                values: new object[,]
+                {
+                    { 2, new DateTime(2019, 4, 22, 0, 0, 0, 0, DateTimeKind.Unspecified), "Avengers Endgame", false },
+                    { 3, new DateTime(2021, 12, 13, 0, 0, 0, 0, DateTimeKind.Unspecified), "Spider-Man: No Way Home", false }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Comments",
+                columns: new[] { "Id", "Content", "MovieId", "Recommend" },
+                values: new object[] { 1, "I love it", 2, true });
+
+            migrationBuilder.InsertData(
+                table: "FilmGenreMovie",
+                columns: new[] { "FilmGenresId", "MoviesId" },
+                values: new object[] { 9, 2 });
+
+            migrationBuilder.InsertData(
+                table: "MoviesActors",
+                columns: new[] { "ActorId", "MovieId", "Character", "SearchOrder" },
+                values: new object[,]
+                {
+                    { 3, 2, "Nick Fury", 2 },
+                    { 3, 3, "Nick Fury", 1 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Actors",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "FilmGenreMovie",
+                keyColumns: new[] { "FilmGenresId", "MoviesId" },
+                keyValues: new object[] { 9, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "MoviesActors",
+                keyColumns: new[] { "ActorId", "MovieId" },
+                keyValues: new object[] { 3, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "MoviesActors",
+                keyColumns: new[] { "ActorId", "MovieId" },
+                keyValues: new object[] { 3, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "Actors",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}

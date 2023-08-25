ffdaace8b6fd31634eed1151ee89c37dd8cdd944
Hellentorresr <htorres1@ucenfotec.ac.cr>
2023-08-24 18:09:25 -0600

    Adding delete methods

--- EFCoreSolution/IntroductionToEFCore/Controllers/FilmGenreController.cs
+++ EFCoreSolution/IntroductionToEFCore/Controllers/FilmGenreController.cs
@@ -88,6 +88,7 @@ namespace IntroductionToEFCore.Controllers
         }
 
         //Deleting using the lettes way of doing it
+        //More efficient because i'm just performing a query which is deleting
         [HttpDelete]
         [Route("DeleteGenre")]
         public async Task<ActionResult> DeleteGenre(int PId)
@@ -99,6 +100,23 @@ namespace IntroductionToEFCore.Controllers
             
            // return Ok();
            return NoContent(); //HTTP response status code 204 No Content is returned by the server to indicate that a HTTP request has been successfully completed, and there is no message body
+        }
+
+        //Deleting using the classic appr
+        //performing two queries 1: finding the record 2: deleting
+        [HttpDelete]
+        [Route("DeleteGenreTwo")]
+        public async Task<ActionResult> DeleteGenreTwo(int PId)
+        {
+            //First finding the Genre with the id == PId
+            var alteredRow = await _dbContext.FilmGenres.FirstOrDefaultAsync(g => g.Id == PId);
+
+            //Then
+            if(alteredRow is null) return NotFound();
+
+            _dbContext.Remove(alteredRow);
+            await _dbContext.SaveChangesAsync();
+            return NoContent();
         }
     }
 }
